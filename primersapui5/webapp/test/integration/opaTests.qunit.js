24c9a8e24bf13ec7d0d919c6098c5385d3066716
jeancarlos16tv <jeancarlos16tv@gmail.com>
2023-10-18 14:50:29 +0000

    first commit on develpment

---
+++ primersapui5/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sap/demo/primersapui5/primersapui5/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ primersapui5/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapdemoprimersapui5/primersapui5/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
